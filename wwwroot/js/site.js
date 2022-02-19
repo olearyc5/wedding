53cdd1c4406859b3429b087182b0c71325f302a6
Conor_PC <olearyc5@tcd.ie>
2022-02-19 22:42:39 +0000

    Improve Mobile responsiveness

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,30 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
+var allowScroll = true;
 // Write your JavaScript code.
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
+
+    if (allowScroll) {
+        disableScroll();
+    } else {
+        enableScroll();
+    }
+}
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
 }
