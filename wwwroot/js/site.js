b3a0fd35cd2a171d4dc6be64dfb57c0046b2af7f
Conor_PC <olearyc5@tcd.ie>
2022-03-05 14:09:26 +0000

    Fixed Homepage CSS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,37 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 
-var allowScroll = true;
-//var menuOpen = false;
-// Write your JavaScript code.
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+//var allowScroll = true;
+////var menuOpen = false;
+//// Write your JavaScript code.
+//function toggleMobileMenu(menu) {
+//    menu.classList.toggle('open');
 
-    if (allowScroll) {
-        //menuOpen = true;
-        disableScroll();
+//    if (allowScroll) {
+//        //menuOpen = true;
+//        disableScroll();
 
-    } else {
-        //menuOpen = false;
-        enableScroll();
-    }
-}
+//    } else {
+//        //menuOpen = false;
+//        enableScroll();
+//    }
+//}
 
-function disableScroll() {
+//function disableScroll() {
 
-    // Get the current page scroll position
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+//    // Get the current page scroll position
+//    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
 
-        // if any scroll is attempted, set this to the previous value
-        window.onscroll = function () {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-    allowScroll = false;
+//        // if any scroll is attempted, set this to the previous value
+//        window.onscroll = function () {
+//            window.scrollTo(scrollLeft, scrollTop);
+//        };
+//    allowScroll = false;
 
-}
+//}
 
-function enableScroll() {
-    window.onscroll = function () { };
-    allowScroll = true;
-}
+//function enableScroll() {
+//    window.onscroll = function () { };
+//    allowScroll = true;
+//}
